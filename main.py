5b53704bd9fef85e30f64db6e02f99a696eee3e0
Santiago <stdelprato@hotmail.com>
2020-10-18 11:05:31 -0300

    Limpieza de codigo

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
+# El script trabaja con ChromeDriver v85 para el navegador Google Chrome v85
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.select import Select
-from pprint import pprint
-import time
 
+# Indicar ruta de donde se encuentra el chromedriver.exe
 PATH = "C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
@@ -19,6 +20,7 @@ try:
         EC.presence_of_element_located((By.CLASS_NAME, "title-info"))
     )
 
+    # Container de la sección de episodios
     seccion_capitulos = WebDriverWait(driver, 10).until(
         EC.presence_of_element_located((By.ID, "section-seasons-and-episodes"))
     )
@@ -39,7 +41,7 @@ try:
     #
     # Luego, automaticamente se agregan al diccionario las sinopsis 
     # de cada uno de los capitulos, separado por temporada.
-    info = {
+    datos_serie = {
         "titulo": tv_show_info.find_element_by_class_name("title-title").text,
         "sinopsis": tv_show_info.find_element_by_class_name("title-info-synopsis").text,
         "anio_de_salida": tv_show_info.find_element_by_class_name("item-year").text,
@@ -48,7 +50,7 @@ try:
         "premios": driver.find_element_by_class_name("hook-text").text
     }
 
-    # Agrego llaves y valores al dict info
+    # Agrego llaves y valores de cada temporada y episodios al dict datos_serie
     for i in range(cantidad_temporadas):
         select_temporada.select_by_index(i)
         temporada_seleccionada = WebDriverWait(driver, 10).until(
@@ -57,20 +59,20 @@ try:
         episodios = temporada_seleccionada.find_elements_by_class_name("episode")
         anio_de_salida = temporada_seleccionada.find_element_by_class_name("season-release-year").text[-4:]
 
-        info[f"temporada_{i+1}_anio_de_salida"] = anio_de_salida
+        datos_serie[f"temporada_{i+1}_anio_de_salida"] = anio_de_salida
         for episodio in episodios:
             episodio_titulo = episodio.find_element_by_class_name("episode-title").text
             episodio_sinopsis = episodio.find_element_by_class_name("epsiode-synopsis").text
 
             if episodio_titulo[1] == ".":
-                info[f"T{i+1}_episodio_{episodios.index(episodio)+1}"] = episodio_titulo[3:]
+                datos_serie[f"T{i+1}_episodio_{episodios.index(episodio)+1}"] = episodio_titulo[3:]
             else:
-                info[f"T{i+1}_episodio_{episodios.index(episodio)+1}"] = episodio_titulo[4:]
+                datos_serie[f"T{i+1}_episodio_{episodios.index(episodio)+1}"] = episodio_titulo[4:]
 
-            info[f"T{i+1}_episodio_{episodios.index(episodio)+1}_sinopsis"] = episodio_sinopsis
+            datos_serie[f"T{i+1}_episodio_{episodios.index(episodio)+1}_sinopsis"] = episodio_sinopsis
     
-    
-    for key, value in info.items():
+    # Imprimo cada valor del diccionario datos_serie
+    for key, value in datos_serie.items():
         print(key, ':', value)
 finally:
     driver.quit()
